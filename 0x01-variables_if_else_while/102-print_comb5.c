cd571fcac290fec732c70a603bccea18fcd5d4c7
alexaorrico <alexaorrico@gmail.com>
2017-05-12 04:23:48 +0000

    fixed for #8 99 condition

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,7 +13,7 @@ int main(void)
 	int i, j, k, l;
 
 	i = j = k = 48; l = 49;
-	while  ((i < 58) && (j < 57) && (k < 58) && (l < 58))
+	while  ((i < 58))
 	{
 		putchar(i); putchar(j); putchar(32); putchar(k); putchar(l);
 		if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
@@ -22,18 +22,21 @@ int main(void)
 		}
 		else
 		{
-			putchar(44);
-			putchar(32);
-			if ((k == 57) && (l == 57) && (j < 57))
+			putchar(44); putchar(32);
+			if ((k == 57) && (l == 57))
 			{
-				l = ++j + 1;
-				k = i;
-			}
-			else if ((k == 57) && (l == 57) && (j == 57))
-			{
-				j = 48;
-				l = 49;
-				k = ++i;
+				if (j < 56)
+				{
+					l = ++j + 1; k = i;
+				}
+				else if (j == 56)
+				{
+					j++; k = i; l = 48;
+				}
+				else if (j == 57)
+				{
+					j = 48; l = 49; k = ++i;
+				}
 			}
 			else if (l < 57)
 			{
@@ -41,8 +44,7 @@ int main(void)
 			}
 			else
 			{
-				l = 48;
-				k++;
+				l = 48; k++;
 			}
 		}
 	}
