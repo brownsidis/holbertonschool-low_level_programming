962b93d89930db78ba732ae22cade3707a3a294c
alexaorrico <alexaorrico@gmail.com>
2017-05-23 19:55:18 +0000

    _atoi function

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ *
+ * Return: value of integer
+ */
+int _atoi(char *s)
+{
+	int i, j, n, m;
+
+	i = n = 0;
+	minus = 1;
+	while (*(s + i))
+	{
+		if ((*(s + i) >= '0') && (*(s + i) <= '9'))
+		{
+			n = n * 10 + (*(s + i) - '0');
+			j = i;
+		}
+		i++;
+	}
+	i = 0;
+	while (i < j)
+	{
+		if (*(s + i) == '-')
+			m *= -1;
+		i++;
+	}
+	return (n * m)
+}
