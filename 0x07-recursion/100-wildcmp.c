4f83aabee22f4359292ebd4aa79ceb09c4400b77
alexaorrico <alexaorrico@gmail.com>
2017-06-02 22:52:17 +0000

    updated wilcompare for all edge cases

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -1,4 +1,23 @@
 #include "holberton.h"
+
+/**
+ * test - tests if wildcard is a proper match
+ * @s5: string to compare
+ * @s6: wildcard match to compare
+ * @t5: first spot that was checked
+ * @t6: first spot in wildcard comparison
+ *
+ * Return: spot that matches or NULL
+ */
+char *test(char *s5, char *s6, char *t5, char *t6)
+{
+	if (*s6 == '*' || (*s5 == '\0' && *s6 == '\0'))
+		return (t5);
+	else if (*s5 != *s6)
+		return (wildcard(s5, t6));
+	else
+		return (test(s5 + 1, s6 + 1, t5, t6));
+}
 /**
  * wildcard - checks for the wildcards
  * @s3: string to compare to
@@ -14,7 +33,7 @@ char *wildcard(char *s3, char *s4)
 			return (0);
 		return (wildcard(s3 + 1, s4));
 	}
-	return (s3);
+	return (test(s3 + 1, s4 + 1, s3, s4));
 }
 
 /**
