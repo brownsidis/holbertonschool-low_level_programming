1f970168281586f3ce6992d96390fb8c5b9b4953
alexaorrico <alexaorrico@gmail.com>
2017-06-06 22:14:45 +0000

    freed memory and condensed loops

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -22,10 +22,11 @@ int **alloc_grid(int width, int height)
 	{
 		a[i] = (int *)malloc(sizeof(int) * width);
 		if (a[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(a[j]);
 			return (NULL);
-	}
-	for (i = 0; i < height; i++)
-	{
+		}
 		for (j = 0; j < width; j++)
 		{
 			a[i][j] = 0;
