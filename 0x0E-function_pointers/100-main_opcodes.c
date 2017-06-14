adc93dbe2b5266826d85bac9d5964998f9c20352
alexaorrico <alexaorrico@gmail.com>
2017-06-14 22:48:19 +0000

    code outline for advanced task

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of pointers to arguments
+ *
+ * Return: always O
+ */
+int main(int argc, char **argv)
+{
+	int n;
+	int (*f)(int, char **);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	n = atoi(argv[1]);
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	f = &main;
+	printf("%p\n", f);
+	return (0);
+}
