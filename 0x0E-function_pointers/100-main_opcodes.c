17eefa9675fa824ab9a88c2abc602bef3f0272f6
alexaorrico <alexaorrico@gmail.com>
2017-06-15 19:49:54 +0000

    added description for print_opcodes

--- 0x0E-function_pointers/100-main_opcodes.c
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ *
+ * Return: void
+ */
 void print_opcodes(char *a, int n)
 {
 	int i;
