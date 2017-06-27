e02a375b3af5b2b21884aa198f9b5757fb13a014
alexaorrico <alexaorrico@gmail.com>
2017-06-27 15:59:46 +0000

    fixed for betty style/doc

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,7 +4,7 @@
 /**
  * insert_nodeint_at_index -  inserts a new node at a given position.
  * @head: double pointer to the first node in the list
- * @index: index of the node to insert
+ * @idx: index of the node to insert
  * @n: value for new node
  *
  * Return: pointer to the indexed node
