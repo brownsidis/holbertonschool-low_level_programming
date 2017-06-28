e1841771898e41c342a4c2f580d6493e74facacf
alexaorrico <alexaorrico@gmail.com>
2017-06-28 20:45:52 +0000

    optimized code

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,17 +16,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (head == NULL || *head == NULL)
 		return (-1);
-	if (index != 0)
-	{
-		current = *head;
-		for (i = 0; i < index - 1 && current->next != NULL; i++)
-		{
-			current = current->next;
-		}
-		if (current->next == NULL)
-			return (-1);
-		next = current->next;
-	}
 	if (index == 0)
 	{
 		next = (*head)->next;
@@ -34,6 +23,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = next;
 		return (1);
 	}
+	current = *head;
+	for (i = 0; i < index - 1; i++)
+	{
+		if (current->next == NULL)
+			return (-1);
+		current = current->next;
+	}
+	next = current->next;
 	current->next = next->next;
 	free(next);
 	return (1);
