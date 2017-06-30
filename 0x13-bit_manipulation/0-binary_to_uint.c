2422eb0be86f6b5c12d29f33f17415cb7583162c
alexaorrico <alexaorrico@gmail.com>
2017-06-30 01:42:49 +0000

    optimized code w/ bit manipulation

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,6 @@
 #include "holberton.h"
 
 /**
- * _pow - calculates a base raides to the exponent power
- * @base: base of the exponential expression
- * @power: exponent
- *
- * Return: result of (base ^ power)
- */
-unsigned int _pow(unsigned int base, unsigned int power)
-{
-	unsigned int i;
-	unsigned int num;
-
-	num = 1;
-	for (i = 1; i <= power; i++)
-	{
-		num *= base;
-	}
-	return (num);
-}
-
-/**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: pointer to a string containing a binary number
  *
@@ -29,7 +9,7 @@ unsigned int _pow(unsigned int base, unsigned int power)
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int num, base, power;
+	unsigned int num;
 
 	num = 0;
 	power = 0;
@@ -41,9 +21,11 @@ unsigned int binary_to_uint(const char *b)
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
-	for (i -= 1; i >= 0; i--, power++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		num += (b[i] - '0') * _pow(base, power);
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
 	}
 	return (num);
 }
