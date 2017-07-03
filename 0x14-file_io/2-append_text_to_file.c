153b06ac8f3950d30d6818b4146232fa2daf6a50
alexaorrico <alexaorrico@gmail.com>
2017-07-03 20:15:31 +0000

    fixed for betty style

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -19,7 +19,13 @@ size_t _strlen(char *str)
 	return (i);
 }
 
-
+/**
+ * append_text_to_file - appends a text at the end of a file.
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success and -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
