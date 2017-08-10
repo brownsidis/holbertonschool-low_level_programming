15e2b578b22dce32bacbd7c49adf501f6c80678e
alexaorrico <alexaorrico@gmail.com>
2017-08-10 07:07:16 +0000

    fix inits in for loops and add last two password chars

--- 0x16-doubly_linked_lists/103-keygen.c
+++ 0x16-doubly_linked_lists/103-keygen.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * main - generate a key depending on a username for crackme5
@@ -22,17 +23,23 @@ int main(int argc, char *argv[])
 	}
 	len = strlen(argv[1]);
 	p[0] = l[(len ^ 59) & 63];
-	for (i = 0; i < len; i++)
+	for (i = 0, add = 0; i < len; i++)
 		add += argv[1][i];
 	p[1] = l[(add ^ 79) & 63];
-	for (i = 0; i < len; i++)
+	for (i = 0, b = 1; i < len; i++)
 		b *= argv[1][i];
 	p[2] = l[(b ^ 85) & 63];
 	for (b = argv[1][0], i = 0; i < len; i++)
-		if (b <= argv[1][i])
+		if ((char)b <= argv[1][i])
 			b = argv[1][i];
 	srand(b ^ 14);
 	p[3] = l[rand() & 63];
+	for (b = 0, i = 0; i < len; i++)
+		b += argv[1][i] * argv[1][i];
+	p[4] = l[(b ^ 239) & 63];
+	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
+		b = rand();
+	p[5] = l[(b ^ 229) & 63];
 	printf("%s\n", p);
 	return (0);
 }
