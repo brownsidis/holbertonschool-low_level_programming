fa59f794f276391cee889d9ff2f64f298ea6e70a
alexaorrico <alexaorrico@gmail.com>
2017-08-10 00:55:51 +0000

    first two letters of password

---
+++ 0x16-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - generate a key depending on a username for crackme5
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int i;
+	size_t len, add;
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char p[7] = "      ";
+
+	if (argc != 2)
+	{
+		printf("Correct usage: ./keygen5 username\n");
+		return (1);
+	}
+	len = strlen(argv[1]);
+	p[0] = l[(len ^ 59) & 63];
+	for (i = 0; len >= i; i++)
+		add += argv[1][i];
+	p[1] = l[(add ^ 79) & 63];
+	printf("%s\n", p);
+	return (0);
+}
