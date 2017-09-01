5a8b3070ffa39b9e7647a6ac002b6d0102032aea
alexaorrico <alexaorrico@gmail.com>
2017-09-01 18:10:57 +0000

    move free for old value back to original position

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -55,10 +55,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
-			free(tmp->value);
 			new_value = strdup(value);
 			if (new_value == NULL)
 				return (0);
+			free(tmp->value);
 			tmp->value = new_value;
 			return (1);
 		}
